ebc7cea46a3a4b7130e6f61b029da4d4b2fed569
Aniket Singh <asingh@hytrust.com>
2019-10-12 00:15:14 +0530

    clean up and added retry on the api

--- finterapp/models.py
+++ finterapp/models.py
@@ -2,6 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class WordsModel(models.Model):
-  word = models.CharField(max_length=255)
-  frequency = models.IntegerField()
--- finterapp/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-class WordsSerializer(serializers.Serializer):
-    word = serializers.CharField(max_length=200)
-    frequency = serializers.IntegerField()
